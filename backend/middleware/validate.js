d35fd0e9a332c32541184a2b470208b986754b14
Milton Franco Ruiz <94766039+miltonfruiz@users.noreply.github.com>
2026-06-29 04:32:06 -0300

    Add backend/middleware/validate.js

---
+++ backend/middleware/validate.js
@@ -0,0 +1,16 @@
+```javascript
+const { check, validationResult } = require('express-validator');
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  next();
+};
+
+module.exports = {
+  check,
+  validate,
+};
+```
